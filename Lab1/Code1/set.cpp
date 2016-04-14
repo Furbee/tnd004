5aa99a3f4908e8d87b64c52e004bf4abe8c1e55c
Oscar Nord <oscar.nord@gmail.com>
2016-04-14 09:30:17 +0200

    started implementing code for lab1

--- Lab1/Code1/set.cpp
+++ Lab1/Code1/set.cpp
@@ -2,15 +2,4 @@
 // Created by Oscar Nord on 07/04/16.
 //
 
-#include "set.h"
-
-
-
-set::set(int) {
-
-
-}
-
-set::set(set R) {}
-
-set::~set() { }
+#include "Set.h"
--- Lab1/Code1/set.h
+++ Lab1/Code1/set.h
@@ -6,14 +6,38 @@
 #define LAB1_SET_H
 
 template< typename T>
-class set {
+class Set {
 
 public:
     // Constructors
-    set(int);
-    set(set R);
+    Set<int> T;
+
+    Set<int> T(v); //create the set R = {v}
+
+    //TODO: Move constructor
+
+    Set<int> T(S); //Copy constructor initializing set R with set S
+
+
     // Destructor
     ~set();
+
+private:
+
+    class Node
+    {
+    public:
+
+        //Constructor
+        Node (T nodeVal = T(), Node* nextPtr = nullptr, Node* prevPtr = nullptr)
+                : value (nodeVal), next (nextPtr), prev(prevPtr)
+        {  };
+
+        //Data members
+        T value;    // Object (element) stored in the Node
+        Node* next; // Pointer to the next Node
+        Node* prev; // Pointer to the previous Node
+    };
 
 
 };
